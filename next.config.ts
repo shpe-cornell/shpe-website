31b46a9fbebc3b7db01ab47ff1424d9f54003c5e
Ruby <rsp227@cornell.edu>
2025-08-24 16:09:37 -0400

    Yuh

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true, // 👈 disables Next.js image optimization for static export
+  },
 };
 
 export default nextConfig;
